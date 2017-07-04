9e67be275d3db140c9e7bf6c9388aeae046a114c
Jan Kvapil <jan@ytec.nl>
2017-07-04 20:27:35 +0200

    initial commit

---
+++ mit/classes/geometry.h
@@ -0,0 +1,14 @@
+class Point {
+    private:
+        int x, y;
+    public:
+        Point() {
+            x = 0;
+            y = 0;
+        }
+        Point(int x, int y) {
+            x = x;
+            y = y;
+        }
+};
+
